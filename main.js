e9552f164172650e394897856ded99ad91e77e47
cjtechnologymz <142857102+cjtechnologymz@users.noreply.github.com>
2023-09-03 19:43:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
+const imageContainer = document.querySelector(".image-container");
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+});
+
+viewer.add(panoramaImage);
